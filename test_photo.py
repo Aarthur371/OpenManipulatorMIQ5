0d7834755e4648c326cad5bd5872ec73058bc55d
El-Bucheron <mat.manducher@gmail.com>
2023-10-02 10:32:52 +0200

    Test2

--- test_photo.py
+++ test_photo.py
@@ -1,4 +1,4 @@
-from picamera import PiCamera
+"""from picamera import PiCamera
 import cv2
 import numpy as np
 camera = PiCamera()
@@ -6,4 +6,20 @@ camera.resolution = (640, 480)
 camera.framerate = 30
 photo = np.empty((640 * 480 * 3), dtype=np.uint8)
 camera.capture(photo, 'bgr')
-cv2.imwrite("Test.png", photo)
+cv2.imwrite("Test.png", photo)"""
+
+import cv2
+
+# open camera
+cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+
+# set dimensions
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1440)
+
+# take frame
+ret, frame = cap.read()
+# write frame to file
+cv2.imwrite('image.jpg', frame)
+# release camera
+cap.release()
