4656afe311edef05b47765b06aae966976985b54
El-Bucheron <mat.manducher@gmail.com>
2023-10-02 10:58:48 +0200

    Ajout code calibration caméra

--- test_photo.py
+++ test_photo.py
@@ -1,13 +1,3 @@
-"""from picamera import PiCamera
-import cv2
-import numpy as np
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 30
-photo = np.empty((640 * 480 * 3), dtype=np.uint8)
-camera.capture(photo, 'bgr')
-cv2.imwrite("Test.png", photo)"""
-
 import cv2
 
 # open camera
