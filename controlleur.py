98ebdf4aec9967be1ef9ecef41b2bd230bcadee4
El-Bucheron <mat.manducher@gmail.com>
2023-11-06 16:06:07 +0100

    Code déplacement + prise de photo

---
+++ controlleur.py
@@ -0,0 +1,54 @@
+
+import sys
+import rospy
+from open_manipulator_msgs.srv import *
+import cv2
+import os
+import numpy as np
+
+
+# release camera
+cap.release()
+
+def set_position_client(x, y, z, time):
+    service_name = '/open_manipulator/goal_task_space_path_position_only'
+
+    rospy.wait_for_service(service_name)
+
+    try:
+        set_position = rospy.ServiceProxy(service_name, SetKinematicsPose)
+
+        arg = SetKinematicsPoseRequest()
+        arg.end_effector_name = 'gripper'
+        arg.kinematics_pose.pose.position.x = x
+        arg.kinematics_pose.pose.position.y = y
+        arg.kinematics_pose.pose.position.z = z
+        arg.path_time = time
+        resp1 = set_position(arg)
+        print('Service done!')
+        return resp1
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+        return False
+
+horizontal_res = 640
+vertical_res = 480
+
+# open camera
+cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, horizontal_res)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, vertical_res)
+
+while(True):
+    reponse = input("Prise de photo -> p nom_photo\nDéplacemet du robot -> d x y z")
+    if reponse != "":
+        tab_reponse = reponse.split(reponse, ' ')
+        if tab_reponse[0] == "p":
+            ret, frame = cap.read()
+            cv2.imwrite(tab_reponse[1] + '.jpg', frame)
+        elif tab_reponse[0] == "d":
+            response = set_position_client(tab_reponse[1], tab_reponse[2], tab_reponse[3], 1)
+        else:
+            print("Commande non reconnue")
+    else:
+        print("Commande vide")
