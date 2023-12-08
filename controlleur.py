e275f45b0c9816f0f2a7cd25242ae2cf245b4595
El-Bucheron <mat.manducher@gmail.com>
2023-12-08 13:48:55 +0100

    flemme de commenter

--- controlleur.py
+++ controlleur.py
@@ -7,12 +7,9 @@ from open_manipulator_msgs.srv import *
 
 def set_position_client(x, y, z, time):
     service_name = '/goal_task_space_path_position_only'
-
     rospy.wait_for_service(service_name)
-
     try:
         set_position = rospy.ServiceProxy(service_name, SetKinematicsPose)
-
         arg = SetKinematicsPoseRequest()
         arg.end_effector_name = 'gripper'
         arg.kinematics_pose.pose.position.x = x
@@ -25,6 +22,26 @@ def set_position_client(x, y, z, time):
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
         return False
+    
+
+def set_relative_angle_client(angle):
+    service_name = '/goal_joint_space_path_from_present'
+    rospy.wait_for_service(service_name)
+    try:
+        set_joint = rospy.ServiceProxy(service_name, SetJointPosition)
+        arg = SetJointPositionRequest()
+        arg.path_time = 1
+        arg.joint_position.joint_name = ['joint1', 'joint2', 'joint3', 'joint4', 'gripper']
+        arg.joint_position.position = angle
+        arg.joint_position.max_accelerations_scaling_factor = 1
+        arg.joint_position.max_velocity_scaling_factor = 1
+        resp1 = set_joint(arg)
+        print('Service done!')
+        return resp1
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
+        return False    
+
 
 horizontal_res = 1920
 vertical_res = 1440
